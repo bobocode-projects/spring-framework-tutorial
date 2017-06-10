0318ae0a018e98ba7f2a7e821656882f05854ef3
Taras <boy4uck@gmail.com>
2017-06-11 00:00:43 +0300

    - Added servlet dependency  - Configured root and web contexts

--- spring-framework-mvc-basics/src/main/java/com/bobocode/controller/HelloController.java
+++ spring-framework-mvc-basics/src/main/java/com/bobocode/controller/HelloController.java
@@ -6,12 +6,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/hello")
+@RequestMapping({"/", "/hello"})
 public class HelloController {
 
     @ResponseBody
     @RequestMapping(method = RequestMethod.GET)
     public String hello() {
-        return "Hello, Bobo!";
+        return "<h1>Hello, Bobo!</h1>";
     }
 }
