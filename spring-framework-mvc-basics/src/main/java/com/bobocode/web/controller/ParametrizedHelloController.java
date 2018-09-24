36695b55f5cdc7dfba2b430fae4798a9728b165f
tboychuk <tarass.boychuk@gmail.com>
2018-09-24 14:07:20 +0300

    Update ParametrizedHelloController.java

--- spring-framework-mvc-basics/src/main/java/com/bobocode/web/controller/ParametrizedHelloController.java
+++ spring-framework-mvc-basics/src/main/java/com/bobocode/web/controller/ParametrizedHelloController.java
@@ -1,23 +1,20 @@
 package com.bobocode.web.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/hello")
 public class ParametrizedHelloController {
 
     @ResponseBody
-    @RequestMapping
+    @GetMapping
     public String helloQueryParam(@RequestParam(defaultValue = "Bobo") String name) {
         return "<h3>Hello, " + name + "</h3>";
     }
 
     @ResponseBody
-    @RequestMapping("/{name}")
+    @GetMapping("/{name}")
     public String helloPathVar(@PathVariable String name) {
         return "<h2>Hello, " + name + "</h2>";
     }
