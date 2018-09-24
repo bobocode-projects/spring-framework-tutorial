9451db723cf1a8f8a1b182ffcef6e110eea4a9f4
tboychuk <tarass.boychuk@gmail.com>
2018-09-24 13:54:42 +0300

    Update AspectsExamples.java

--- spring-framework-aop-basics/src/main/java/com/bobocode/AspectsExamples.java
+++ spring-framework-aop-basics/src/main/java/com/bobocode/AspectsExamples.java
@@ -1,16 +1,23 @@
 package com.bobocode;
 
 import com.bobocode.configs.ApplicationConfigs;
+import com.bobocode.model.User;
 import com.bobocode.service.UserService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
 
 public class AspectsExamples {
     private static UserService userService;
 
     public static void main(String[] args) {
         init();
-        userService.findAllUsers().forEach(System.out::println);
+        List<User> users = userService.findAllUsers();
+        User user = userService.findByEmail("test@gmail.com");
+
+        System.out.printf("All users: %s%n", users);
+        System.out.printf("User found by email:%s%n", user);
     }
 
     private static void init() {
--- spring-framework-aop-basics/src/main/java/com/bobocode/aspects/UserServiceAspects.java
+++ spring-framework-aop-basics/src/main/java/com/bobocode/aspects/UserServiceAspects.java
@@ -12,9 +12,9 @@ import java.util.List;
 @Component
 public class UserServiceAspects {
 
-    @Before("execution(* com.bobocode.service.UserService.findByEmail(String)) && args(email)")
+    @Before("execution(com.bobocode.model.User com.bobocode.service.UserService.findByEmail(String)) && args(email)")
     public void beforeUserFindByEmail(String email) {
-        System.out.println("*** AOP *** Find user by email: " + email);
+        System.out.println("*** Find user by email: " + email);
     }
 
     @Pointcut("execution(* com.bobocode.service.UserService.find*(..))")
@@ -23,19 +23,20 @@ public class UserServiceAspects {
 
     @Before("findFromUserService()")
     public void beforeFind() {
-        System.out.println("*** AOP - BEFORE *** Find from UserService");
+        System.out.println("*** Find from UserService");
     }
 
     @After("findFromUserService() && target(service)")
     public void afterFindUser(JoinPoint joinPoint, UserService service) {
         String methodName = joinPoint.getSignature().getName();
         String className = service.getClass().getName();
-        System.out.println("*** AOP - AFTER *** Got information using method " + methodName + " of class " + className);
+        System.out.println("*** Got information using method " + methodName + " of class " + className);
     }
 
     @AfterReturning(value = "execution(* com.bobocode.service.UserService.findAllUsers(..))", returning = "users")
     public void afterReturningUserList(List<User> users) {
-        System.out.println("*** AOP - AFTER RETURNING *** Returned " + users.size() + " users");
+        System.out.println("*** Returned " + users.size() + " users");
+
     }
 
 }
